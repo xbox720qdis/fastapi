0f14a2264d53aa6e0494aea09a88823728b9a414
xbox720qdis <xbox720qds@gmail.com>
2024-11-18 12:47:13 +0100

    In the beninging

---
+++ main.py
@@ -0,0 +1,49 @@
+from uuid import UUID
+from typing import List
+from models import User, Gender, Role
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+db: List[User] = [
+   User(
+       id=UUID("99914afc-d48a-4b1b-8e1e-492d32d2cf03"),
+       first_name="beni",
+        last_name="ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+       id=UUID("37db47f8-6abf-4c1e-aff7-44d8a4e84c16"),
+       first_name="sikh",
+        last_name="Nigga Balls",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
+]
+@app.get("/")
+async def root():
+    return {"hello": "mundo"}  
+
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return 
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exist"
+    )
+
+        
---
+++ models.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
+from enum import Enum
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    student = "student"
+
+class User(BaseModel):
+    id: UUID = Field(default_factory=uuid4)  # Correct UUID generation
+    first_name: str
+    last_name: str
+    middle_name: Optional[str] = None  # Optional field with a default value
+    gender: Gender
+    roles: List[Role]
